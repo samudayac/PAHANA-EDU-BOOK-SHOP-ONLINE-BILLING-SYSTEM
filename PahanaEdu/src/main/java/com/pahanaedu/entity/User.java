c83616e79c913127bfd419b861553e285f2265be
Samudaya Chandrasekara <samudayachandrasekara@gmail.com>
2025-08-19 09:57:46 +0530

    User Model Added
    User.java

---
+++ PahanaEdu/src/main/java/com/pahanaedu/entity/User.java
@@ -0,0 +1,71 @@
+package com.pahanaedu.entity;
+
+
+public class User {
+    
+    private int id;
+    private String name;
+    private String email;
+    private String contactno;
+    private String address;
+    private String password;
+
+    public User() {
+        super();
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContactno() {
+        return contactno;
+    }
+
+    public void setContactno(String contactno) {
+        this.contactno = contactno;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+        @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name=" + name + ", email=" + email + ", contactno=" + contactno + ", address =" + address + ", password=" + password + '}';
+    }
+      
+}
